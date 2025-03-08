824866bc3b5b6a8ac0dc0eb19a5bda8540610e90
znyan <yanzhn@outlook.com>
2025-03-08 14:59:55 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/freehand-3d-us-demo',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
